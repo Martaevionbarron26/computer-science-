567b28d41aeb1a537ac0a90ca6eabd94032cfdb5
Martaevionbarron26 <tae2889@yahoo.com>
2024-09-26 14:50:53 -0500

    date

--- Assigments/Animal Survey.py
+++ Assigments/Animal Survey.py
@@ -7,3 +7,5 @@ print("You entered: " + user_input3)
 user_input4 = input("what is a fun fact about them ")
 print("You entered: " + user_input4)
 print("So your favorite animal is " + user_input1 + "? and you like theme because? " + user_input2 + " where do they live " + user_input3 + " what is a fun fact about them " + user_input4)
+#i ilke dogs to 
+'''I HATE DOGS '''
