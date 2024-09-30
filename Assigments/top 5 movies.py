27bfea1e1bf559353e08d87e6526701d25cad8ec
Martaevionbarron26 <tae2889@yahoo.com>
2024-09-30 14:56:00 -0500

    top 5 movies

---
+++ Assigments/top 5 movies.py
@@ -0,0 +1,9 @@
+
+def top_movies(movie1, movie2, movie3, movie4, movie5): #topmovies
+    print("1." + movie1)
+    print("2. " + movie2)
+    print("3. " + movie3)
+    print("4. " + movie4)
+    print("5. " + movie5)
+    
+movies = top_movies("Littel man", "spider man 3", "friday", "batman movie", "step the yard")
