b339105e6ef68e2fbd85d837ffc66f35bcfcd711
Martaevionbarron26 <tae2889@yahoo.com>
2024-09-30 15:18:59 -0500

    simple calc

--- Assigments/simple calaulation.py
+++ Assigments/simple calaulation.py
@@ -1,2 +1,19 @@
 def add(x, y):
     print(x + y)
+
+def subtract(x, y):
+    print(x - y)
+
+def muliply(x, y):
+    print(x * y)
+
+def divide(x, y):
+    print(x / y)
+
+add(5,5)
+
+subtract(8,10)
+
+divide(100,10)
+
+muliply(3,10)
