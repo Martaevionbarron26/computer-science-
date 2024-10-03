f16525e710d4391421632e74dd39c53fa3453160
Martaevionbarron26 <tae2889@yahoo.com>
2024-10-03 15:09:02 -0500

    unit 2 test

---
+++ unit 2 test/add_tree.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    print(x + y)
+
+
+def subtract(x, y):
+    print(x - y)
+
+def multiply(x, y):
+    print(x * y)
+
+
+add(5, 5)
+
+subtract(1000, 900)
+
+multiply(8, 9)
---
+++ unit 2 test/three inputs.py
@@ -0,0 +1,7 @@
+print("whats your favriot game survay")
+
+game = input("Whats your favrit game? ")
+Why = input ("why is it your favrit game? ")
+when = input ("when did you first start playing the game? ")
+
+print(" whats your favrit game? " + game + " why is it your favriot game? " + Why + " when did you first start playing this game? " + when)
