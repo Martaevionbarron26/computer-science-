565da3baa7dcfe5b1e1bfc8a2d222ffeffe0d3ef
Martaevionbarron26 <tae2889@yahoo.com>
2024-10-08 14:55:19 -0500

    boolean quiz

---
+++ Assigments/Boolean Expressions Quiz.py
@@ -0,0 +1,17 @@
+print("boolean expressions Quiz")
+
+answer = int(input("give a whole number that is less than 5.\n"))
+print(answer<5)
+
+
+answer2 = int(input("give a whole number that is more than 30.\n"))
+print(answer2>30)
+
+answer3 = int(input("give a whole number that is less than 1.\n"))
+print(answer3<1)
+
+answer4 = int(input("give a whole number that is less than .1.\n"))
+print(answer4<.1)
+
+answer5 = int(input("give a whole number that is more than -10.\n"))
+print(answer5<-10)
